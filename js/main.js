1d8dc639a6450fe60308f99fd9a2971be1d14fae
Tom Gurion <nagasaki45@gmail.com>
2019-05-21 13:00:25 +0100

    Even simple things are harder with JS :-(

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function formatDate(date) {
   var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
                     'Sep', 'Oct', 'Nov', 'Dec'];
   return date.getDate() + ' ' +
-         monthNames[date.getMonth() - 1] + ' ' +
+         monthNames[date.getMonth()] + ' ' +
 	       date.getFullYear();
 }
 
@@ -20,27 +20,27 @@ new Vue({
     yesterday: getYesterday(),
     gigs: [
       {
-	      when: new Date(2018, 3, 21),
+	      when: new Date(2018, 2, 21),
 	      content: '<a href="https://www.facebook.com/events/511121392622338/">ElectrolightsAV VI</a><br >Rye Wax',
       },
       {
-	      when: new Date(2018, 9, 29),
+	      when: new Date(2018, 8, 29),
 	      content: '<a href="https://therawandthecooked.intersections.io/cooked.html">The RAW and The COOKED</a><br >Café 1001'
       },
       {
-	      when: new Date(2018, 10, 18),
+	      when: new Date(2018, 9, 18),
 	      content: '<a href="https://www.residentadvisor.net/events/1169850">ElectrolightsAV XII</a><br >New River Studios'
       },
       {
-	      when: new Date(2019, 2, 23),
+	      when: new Date(2019, 1, 23),
 	      content: '<a href="https://www.meetup.com/London-modular-synthesis-group/events/258296806/">CV Freqs XI</a><br >IKLECTIK'
       },
       {
-	      when: new Date(2019, 5, 1),
+	      when: new Date(2019, 4, 1),
 	      content: '<a href="https://www.residentadvisor.net/events/1204412">Sonic Electronics</a><br >The others'
       },
       {
-	      when: new Date(2019, 6, 15),
+	      when: new Date(2019, 5, 15),
               content: 'As <b>IDMT?</b> w/ <b><a href="https://lwlsn.github.io/">digital selves</a></b><br >ElectrolightsAV<br >New River Studios'
       },
     ]
